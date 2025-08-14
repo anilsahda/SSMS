eeb03c84775411a49855433601587ab654a0a217
Anil <anilbca1996@gmail.com>
2025-08-14 10:57:32 +0530

    image upload

--- SSMS.API/Controllers/StudentsController.cs
+++ SSMS.API/Controllers/StudentsController.cs
@@ -2,7 +2,6 @@
 using SSMS.API.Data;
 using SSMS.API.Data.Entitities;
 using SSMS.API.DTOs;
-using System.IO;
 
 namespace SSMS.API.Controllers
 {
@@ -11,12 +10,10 @@ namespace SSMS.API.Controllers
     public class StudentsController : ControllerBase
     {
         private readonly AppDbContext _context;
-        private readonly IWebHostEnvironment _env;
 
         public StudentsController(AppDbContext context, IWebHostEnvironment env)
         {
             _context = context;
-            _env = env;
         }
 
         [HttpGet]
@@ -52,6 +49,55 @@ namespace SSMS.API.Controllers
 
         [HttpPut("{id}")]
         public IActionResult UpdateStudent(int id, [FromBody] StudentDTO studentdto)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound("Student not found");
+            }
+
+            student.FirstName = studentdto.FirstName;
+            student.MiddleName = studentdto.MiddleName;
+            student.LastName = studentdto.LastName;
+            student.Address = studentdto.Address;
+            student.Email = studentdto.Email;
+            student.Mobile = studentdto.Mobile;
+            student.CountryId = studentdto.CountryId;
+            student.StateId = studentdto.StateId;
+            student.DistrictId = studentdto.DistrictId;
+            student.GenderId = studentdto.GenderId;
+            student.Image = studentdto.Image;
+
+            _context.SaveChanges();
+            return Ok(student);
+        }
+
+        [HttpPost("AddImageUpload")]
+        public IActionResult AddImageUpload([FromBody] StudentDTO studentdto)
+        {
+            var student = new Student
+            {
+                FirstName = studentdto.FirstName,
+                MiddleName = studentdto.MiddleName,
+                LastName = studentdto.LastName,
+                Address = studentdto.Address,
+                Email = studentdto.Email,
+                Mobile = studentdto.Mobile,
+                CountryId = studentdto.CountryId,
+                StateId = studentdto.StateId,
+                DistrictId = studentdto.DistrictId,
+                GenderId = studentdto.GenderId,
+                Image = studentdto.Image
+            };
+
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+            return Ok(student);
+        }
+
+        [HttpPut("UpdateImageUpload/{id}")]
+        public IActionResult UpdateImageUpload(int id, [FromBody] StudentDTO studentdto)
         {
             var student = _context.Students.Find(id);
             if (student == null)
